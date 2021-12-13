a241676647572916f284956ece5db24f0ce12f4d
Cleydson <32269793+insanniity@users.noreply.github.com>
2021-12-12 23:51:17 -0300

    Update settings.py

--- python/ediaristas/ediaristas/settings.py
+++ python/ediaristas/ediaristas/settings.py
@@ -39,15 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'easy_mask',
+    'cors-headers',
     'web',
     'api',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
